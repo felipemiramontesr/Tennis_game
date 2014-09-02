056852249e19012c03f4c0feb1e4f4df0e0c8390
Felipe Miramontes Romero <felipemiramontesr@gmail.com>
2014-09-02 13:15:59 -0500

    Correccion de pruebas

--- Game.py
+++ Game.py
@@ -15,22 +15,22 @@ class Game:
   >>> game.scores(1).scores(1).scores(1).score()
   [40, 0]
   >>> game = Game()
-  >>> game.scores(1).scores(1).scores(2).score(2).score()
+  >>> game.scores(1).scores(1).scores(2).scores(2).score()
   Deuce
   >>> game = Game()
-  >>> game.scores(1).scores(1).scores(1).score(2).score(2).score(2).score()
+  >>> game.scores(1).scores(1).scores(1).scores(2).scores(2).scores(2).score()
   [40, 40]
   >>> game = Game()
-  >>> game.scores(1).scores(1).scores(1).score(2).score(2).score(2).scores(1).score()
+  >>> game.scores(1).scores(1).scores(1).scores(2).scores(2).scores(2).scores(1).score()
   Advantage player 1
   >>> game = Game()
-  >>> game.scores(1).scores(1).scores(1).score(2).score(2).score(2).scores(1).scores(2).score()
+  >>> game.scores(1).scores(1).scores(1).scores(2).scores(2).scores(2).scores(1).scores(2).score()
   Deuce
   >>> game = Game()
-  >>> game.scores(1).scores(1).scores(1).score(2).score(2).score(2).scores(1).scores(2).scores(2).score()
+  >>> game.scores(1).scores(1).scores(1).scores(2).scores(2).scores(2).scores(1).scores(2).scores(2).score()
   Advantage player 2
   >>> game = Game()
-  >>> game.scores(1).scores(1).scores(1).score(2).score(2).score(2).scores(1).scores(2).scores(2).scores(2).score()
+  >>> game.scores(1).scores(1).scores(1).scores(2).scores(2).scores(2).scores(1).scores(2).scores(2).scores(2).score()
   The winner is player 2
   """
 
